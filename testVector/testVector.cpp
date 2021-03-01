e2e35c03bcabfd48836d1a0a8609c45bdb3cd246
xjr01 <1900012131@pku.edu.cn>
2021-03-01 22:46:04 +0800

    增加 test 项目

---
+++ testVector/testVector.cpp
@@ -0,0 +1,39 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector2d.h>
+#include <EasyX_Draw.h>
+#include <Windows.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testVector
+{
+	TEST_CLASS(testVector)
+	{
+	public:
+		
+		TEST_METHOD(TestAdd)
+		{
+			Vector2D a(2, 3), b(4, 5);
+			Assert::IsTrue(a + b == Vector2D(6, 8));
+		}
+	};
+	TEST_CLASS(testDraw) {
+	public:
+
+		TEST_METHOD(TestDraw) {
+			createWin();
+			BeginBatchDraw();
+			setCurrentColor(Color(0, 0, 127));
+			drawLine(Segment(Vector2D(100, 80), Vector2D(200, 380)), 10);
+			drawCircle(Vector2D(100, 500), 120, 8);
+			drawCircle(Vector2D(1300, 500), 120, 1.5);
+			drawSolidCircle(Vector2D(500, 400), 120);
+			drawDot(Vector2D(900, 180), 9);
+			FlushBatchDraw();
+			EndBatchDraw();
+			Sleep(5000);
+			exit(0);
+		}
+	};
+}
