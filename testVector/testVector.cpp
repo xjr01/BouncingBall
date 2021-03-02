cd10af74173ecc3a7790a0da9e00436f11438438
xjr01 <1900012131@pku.edu.cn>
2021-03-02 13:51:29 +0800

    鼠标点击绘制Solid多边形（非常慢）

--- testVector/testVector.cpp
+++ testVector/testVector.cpp
@@ -8,7 +8,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace testVector
 {
-	TEST_CLASS(testVector)
+	/*TEST_CLASS(testVector)
 	{
 	public:
 		
@@ -17,11 +17,11 @@ namespace testVector
 			Vector2D a(2, 3), b(4, 5);
 			Assert::IsTrue(a + b == Vector2D(6, 8));
 		}
-	};
+	};*/
 	TEST_CLASS(testDraw) {
 	public:
 
-		TEST_METHOD(TestDraw) {
+		/*TEST_METHOD(TestDraw) {
 			createWin();
 			BeginBatchDraw();
 			setCurrentColor(Color(0, 0, 127));
@@ -38,6 +38,46 @@ namespace testVector
 			while (!GetAsyncKeyState(VK_ESCAPE));
 			closegraph();
 			exit(0);
+		}*/
+
+		TEST_METHOD(TestPolygonDraw) {
+			createWin();
+			BeginBatchDraw();
+			cleardevice();
+			setCurrentColor(Color(0, 0, 127));
+			FlushBatchDraw();
+
+			MOUSEMSG mousemsg{};
+			std::vector<PolygonClass> polygons;
+			PolygonClass cur_polygon;
+			int lst_time = clock(), cur_time;
+
+			PeekMouseMsg(&mousemsg, false);
+			cur_polygon.vertices.push_back(Vector2D(mousemsg.x, mousemsg.y));
+			while (!GetAsyncKeyState(VK_ESCAPE)) {
+				bool mousehit = PeekMouseMsg(&mousemsg);
+				cur_polygon.vertices[cur_polygon.vertices.size() - 1] = Vector2D(mousemsg.x, mousemsg.y);
+				if (mousehit) {
+					if (mousemsg.uMsg == WM_LBUTTONDOWN) {
+						cur_polygon.vertices.push_back(Vector2D(mousemsg.x, mousemsg.y));
+					}
+					if (mousemsg.uMsg == WM_RBUTTONDOWN) {
+						polygons.push_back(cur_polygon);
+						cur_polygon.vertices = std::vector<Vector2D>(1);
+					}
+				}
+
+				cur_time = clock();
+				if (cur_time - lst_time >= 100) {
+					lst_time = cur_time;
+					cleardevice();
+					for (auto p : polygons) drawSolidPolygon(p);
+					drawSolidPolygon(cur_polygon);
+					FlushBatchDraw();
+				}
+			}
+			EndBatchDraw();
+			closegraph();
 		}
 	};
 }
