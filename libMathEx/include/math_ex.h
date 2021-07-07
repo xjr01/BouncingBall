00d82cf7bff93d65ac9f79a430855211517bed7d
xjr01 <1900012131@pku.edu.cn>
2021-07-08 00:40:49 +0800

    增大eps以提高性能

--- libMathEx/include/math_ex.h
+++ libMathEx/include/math_ex.h
@@ -6,7 +6,7 @@
 extern const double pi;
 
 namespace DOUBLE_EPS {
-	const double eps = 1e-6;
+	const double eps = 1e-3;
 	bool eq(double a, double b);
 	bool neq(double a, double b);
 	bool lt(double a, double b);
